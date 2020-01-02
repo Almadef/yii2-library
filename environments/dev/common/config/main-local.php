361ff8cb3e9dc2df8f1a6e9383de59f8f08d5436
Chuvashov Mikhail <kaberton@outlook.com>
2020-01-02 14:35:20 +0400

    init

---
+++ environments/dev/common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=mysql;dbname=yii2-library',
+            'username' => 'user',
+            'password' => 'user123',
+            'charset' => 'utf8',
+            'tablePrefix' => 'yii2-library_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
